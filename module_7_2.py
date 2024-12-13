4a83785f3eaf67c4d485399d3f67638d40e63c1e
Purple Caramel <124286350+bbycaramel@users.noreply.github.com>
2024-12-13 14:48:42 +0300

    Update module_7_21.py

--- module_7_2.py
+++ module_7_2.py
@@ -3,17 +3,14 @@ def custom_write(file_name, strings):
 
     with open(file_name, 'w', encoding='utf-8') as file:
         for index, string in enumerate(strings, start=1):
-            # Получаем текущую позицию в байтах
             byte_position = file.tell()
-            # Записываем строку в файл с добавлением символа новой строки
             file.write(string + '\n')
-            # Сохраняем позицию и строку в словаре
             strings_positions[(index, byte_position)] = string
 
     return strings_positions
 
 
-# Пример выполняемого кода
+# Пример
 info = [
     'Text for tell.',
     'Используйте кодировку utf-8.',
